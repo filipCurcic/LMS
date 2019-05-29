8df45abb6d3a6c8862a96e13c892cf570643f3d2
jakovMitrovic <jaki.mitrovic@gmail.com>
2019-05-29 20:38:39 +0200

    prikaz ispita

---
+++ src/main/java/app/controllers/ExamController.java
@@ -0,0 +1,52 @@
+package app.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import app.entities.Exam;
+import app.services.ExamService;
+
+@Controller
+@RequestMapping("/exam")
+public class ExamController {
+	
+	@Autowired
+	ExamService exSer;
+	
+	
+	@RequestMapping("/all")
+	public ResponseEntity<Iterable<Exam>> getExams() {
+		return new ResponseEntity<Iterable<Exam>>(exSer.getAll(), HttpStatus.OK);
+	}
+
+	@RequestMapping(value = "/add", method = RequestMethod.POST)
+	public ResponseEntity<Exam> addExam(@RequestBody Exam exam) {
+		exSer.addExam(exam);
+		return new ResponseEntity<Exam>(exam, HttpStatus.OK);
+	}
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ResponseEntity<Exam> getExam(@PathVariable Long id) {
+		
+		return new ResponseEntity<Exam>(HttpStatus.NOT_FOUND);
+		
+		
+	}
+
+	@RequestMapping(value="/{id}", method=RequestMethod.DELETE)
+	public ResponseEntity<Exam> removeExam(@PathVariable Long id) {
+		try {
+			exSer.removeExam(id);
+		} catch (Exception e) {
+			return new ResponseEntity<Exam>(HttpStatus.NOT_FOUND);
+		}
+		return new ResponseEntity<Exam>(HttpStatus.NO_CONTENT);
+	}
+
+}
---
+++ src/main/java/app/entities/Exam.java
@@ -0,0 +1,126 @@
+package app.entities;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.NotNull;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+
+
+@Entity
+public class Exam {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
+	
+	@JsonFormat(pattern="yyyy-MM-dd")
+	private Date startTime;
+	
+	@NotNull
+	private String name;
+	
+	@JsonFormat(pattern="yyyy-MM-dd")
+	private Date endTime;
+	@NotNull
+	private int points;
+	
+	private String examTerm;
+	
+	private int grade;
+	
+	private int duration;
+
+
+	public Exam() {}
+
+	public Exam(String name, Date startTime, Date endTime, int points, int duration, String examTerm, int grade) {
+		super();
+		this.name = name;
+		this.startTime = startTime;
+		this.endTime = endTime;
+		this.points = points;
+		this.duration = duration;
+		this.grade = grade;
+	}
+
+	public int getGrade() {
+		return grade;
+	}
+
+	public void setGrade(int grade) {
+		this.grade = grade;
+	}
+	
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+
+	public String getExamTerm() {
+		return examTerm;
+	}
+
+	public void setExamTerm(String examTerm) {
+		this.examTerm = examTerm;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Date getStartTime() {
+		return startTime;
+	}
+
+	public void setStartTime(Date startTime) {
+		this.startTime = startTime;
+	}
+
+	public Date getEndTime() {
+		return endTime;
+	}
+
+	public void setEndTime(Date endTime) {
+		this.endTime = endTime;
+	}
+
+	public int getPoints() {
+		return points;
+	}
+
+	public void setPoints(int points) {
+		this.points = points;
+	}
+
+	public int getDuration() {
+		return duration;
+	}
+
+	public void setDuration(int duration) {
+		this.duration = duration;
+	}
+
+
+	
+
+
+	
+}
